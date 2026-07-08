8baf51722ffab6197cea9783e6546754f427b136
PrithvirajVNair <prithvirajvnair.pvr@gmail.com>
2026-07-08 19:07:07 +0530

    fix :Fixed an production error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import FlagQuiz from "./pages/FlagQuiz";
+import PNF from "./pages/PNF";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<FlagQuiz />} />
+
+
+        
+        <Route path="*" element={<PNF />} />
       </Routes>
     </>
   );
